2af05f182fc5abbec21dd246c032bf7db73d8b91
onlihernan <onlihernan@gmail.com>
2023-04-07 18:07:18 -0300

    commit from asus pc

---
+++ cypress/support/step_definitions/Homepage_Steps.js
@@ -0,0 +1,11 @@
+import { Given, When } from "@badeball/cypress-cucumber-preprocessor";
+
+const url = "http://www.webdriveruniversity.com";
+
+Given("I navigate to the webdriveruniversity homepage", () => {
+    cy.visit(url);
+}) 
+
+// When("I click on the contact us button", () => {
+//     cy.get();
+// })
