05a004e3fba4447c60807f7642e81d7e141e69cc
onlihernan <onlihernan@gmail.com>
2023-04-08 15:12:19 -0300

    3rd scenario with asus - branches correction

--- cypress/support/step_definitions/Homepage_Steps.js
+++ cypress/support/step_definitions/Homepage_Steps.js
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-import { Given, When } from "@badeball/cypress-cucumber-preprocessor";
-
-const url = "http://www.webdriveruniversity.com";
-
-Given("I navigate to the webdriveruniversity homepage", () => {
-    cy.visit(url);
-}) 
-
-// When("I click on the contact us button", () => {
-//     cy.get();
-// })
-=======
 /// <reference types="cypress" />
 import { Given, When } from "@badeball/cypress-cucumber-preprocessor";
 
@@ -23,4 +10,3 @@ Given("I navigate to the webdriveruniversity homepage", () => {
 When("I click on the contact us button", () => {
     cy.get("#contact-us").invoke("removeAttr", "target").click();
 })
->>>>>>> 8e7968c4891483f5e00b561af48b1f0cf2de5fe6
